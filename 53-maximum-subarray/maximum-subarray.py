1945d2dfe36e701cb7bd2fedd9aace137a250fda
Nehan__ <145008649+NEHANGRM@users.noreply.github.com>
2026-08-03 20:52:39 +0530

    Time: 20 ms (98.93%) | Memory: 21.4 MB (26.47%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        s=0
+        m=float('-inf')
+        for i in range(len(nums)):
+            s+=nums[i]
+            if s>m:
+                m=s
+            if s<0:
+                s=0
+        return m
+        
