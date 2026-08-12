b0b57a840c675aecf59fa0f992b578d4d4a7c61d
Nehan__ <145008649+NEHANGRM@users.noreply.github.com>
2026-08-13 02:47:22 +0530

    Time: 35 ms (43.54%) | Memory: 14.1 MB (51.12%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        i=0
+        j=0
+        while i<len(s) and j<len(g):
+            if s[i]>=g[j]:
+                j+=1
+            i+=1
+        return j
+
+        
