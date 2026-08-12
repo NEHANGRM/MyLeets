cce782c11d6bb33a4b653b796b421a1069ce9fa8
Nehan__ <145008649+NEHANGRM@users.noreply.github.com>
2026-08-13 01:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (58.51%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,19 +1,11 @@
 class Solution(object):
     def rob(self, nums):
         n=len(nums)
-        dp=[-1]*(n+1)
-        return self.solve(nums,n-1,dp)
-
-    def solve(self,nums,i,dp):
-        if i<0:
-            return 0
-        if i==0:
+        if n==1:
             return nums[0]
-        if dp[i]!=-1:
-            return dp[i]
-        pick=nums[i]+self.solve(nums,i-2,dp)
-        notpick=self.solve(nums,i-1,dp)
-        dp[i]=max(pick,notpick)
-        return dp[i]
-
-
+        dp=[0]*(n+1)
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+        return dp[n-1]
