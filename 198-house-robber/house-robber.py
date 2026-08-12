4b6df9e1bcb4e96af31ca2cd6a23b07a2454d3ba
Nehan__ <145008649+NEHANGRM@users.noreply.github.com>
2026-08-13 01:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (21.15%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,7 +3,7 @@ class Solution(object):
         n=len(nums)
         if n==1:
             return nums[0]
-        dp=[0]*(n+1)
+        dp=[0]*(n)
         dp[0]=nums[0]
         dp[1]=max(nums[0],nums[1])
         for i in range(2,n):
